24fc77f8f06f4e98612eceed5cdb79ea281cc562
Austin Buhler <austinbuhler@gmail.com>
2020-04-22 20:41:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,15 +5,8 @@ def load_library(path)
   file = File.read(path)
   emoticons = YAML.load(file).to_h
 
-  emoticons.each_pair do |key, value|
-    emoticons[key].each do |x|
-      puts {:key => {
-        :english => x[0],
-        :japanese => x[1]
-        }
-      }
-    end
-  end
+
+
 end
 
 binding.pry
@@ -25,3 +18,10 @@ end
 def get_english_meaning
   # code goes here
 end
+
+emoticons.each_pair do |key, value|
+  emoticons[key].each do |x|
+    puts {:key => {
+      :english => x[0],
+      :japanese => x[1]
+      }
